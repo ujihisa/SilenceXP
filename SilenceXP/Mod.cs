e798325ee2a007c1f7a58525cfb39f999c31de18
ujihisa <ujihisa@gmail.com>
2024-07-11 10:16:09 -0700

    Add project files.
    It worked

---
+++ SilenceXP/Mod.cs
@@ -0,0 +1,78 @@
+﻿using Colossal.IO.AssetDatabase;
+using Colossal.Logging;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+using HarmonyLib;
+using static Game.Rendering.Debug.RenderPrefabRenderer;
+using Unity.Entities;
+using HarmonyLib.Tools;
+using UnityEngine;
+using System.Reflection;
+using Colossal.IO.AssetDatabase.Internal;
+
+
+namespace SilenceXP
+{
+    public partial class Mod : GameSystemBase, IMod
+    {
+        public static ILog log = LogManager.GetLogger($"{nameof(SilenceXP)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
+        private Setting m_Setting;
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            log.Info(nameof(OnLoad));
+
+            if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+                log.Info($"Current mod asset at {asset.path}");
+
+            m_Setting = new Setting(this);
+            m_Setting.RegisterInOptionsUI();
+            GameManager.instance.localizationManager.AddSource("en-US", new LocaleEN(m_Setting));
+
+            AssetDatabase.global.LoadSettings(nameof(SilenceXP), m_Setting, new Setting(this));
+
+            // Add yourself, so Harmonyx prefix can find your settings.
+            // If you can directly refer the setting that would be even better though...
+            World.DefaultGameObjectInjectionWorld.AddSystemManaged<Mod>(this);
+
+            // Actual method patching
+            Harmony.CreateAndPatchAll(typeof(Mod));
+            Harmony.GetAllPatchedMethods().ForEach(m => {
+                log.Warn($"Patched: {m.Name}");
+            });
+        }
+
+        public void OnDispose()
+        {
+            log.Info(nameof(OnDispose));
+            if (m_Setting != null)
+            {
+                m_Setting.UnregisterInOptionsUI();
+                m_Setting = null;
+            }
+        }
+
+        // for GameSystemBase
+        protected override void OnUpdate() { }
+
+        public Setting Setting => m_Setting;
+
+        // namespace 
+        // 	public class MilestoneUISystem : UISystemBase, IXPMessageHandler
+        // public void AddMessage(XPMessage message)
+        [HarmonyPatch(typeof(Game.UI.InGame.MilestoneUISystem), "AddMessage")]
+        [HarmonyPrefix]
+        static bool PatchedAddMessage(Game.UI.InGame.MilestoneUISystem __instance)
+        {
+            log.Debug("PatchedAddMessage");
+            var mod = __instance.World.GetExistingSystemManaged<SilenceXP.Mod>();
+            if (mod == null) {
+                log.Warn("mod does not exist");
+                return true;
+            }
+            log.Debug($"mod.Setting.Toggle: {mod.Setting.Toggle}");
+            return !mod.Setting.Toggle;
+        }
+    }
+}
---
+++ SilenceXP/Setting.cs
@@ -0,0 +1,56 @@
+﻿using Colossal;
+using Colossal.IO.AssetDatabase;
+using Game.Modding;
+using Game.Settings;
+using Game.UI;
+using Game.UI.Widgets;
+using System.Collections.Generic;
+
+namespace SilenceXP
+{
+    [FileLocation(nameof(SilenceXP))]
+    [SettingsUIGroupOrder(kToggleGroup)]
+    [SettingsUIShowGroupName( kToggleGroup)]
+    public class Setting : ModSetting
+    {
+        public const string kSection = "Main";
+
+        public const string kToggleGroup = "Toggle";
+
+        public Setting(IMod mod) : base(mod) {
+        }
+
+        [SettingsUISection(kSection, kToggleGroup)]
+        public bool Toggle { get; set; }
+
+        public override void SetDefaults()
+        {
+            Toggle = true;
+        }
+    }
+
+    public class LocaleEN : IDictionarySource
+    {
+        private readonly Setting m_Setting;
+        public LocaleEN(Setting setting)
+        {
+            m_Setting = setting;
+        }
+        public IEnumerable<KeyValuePair<string, string>> ReadEntries(IList<IDictionaryEntryError> errors, Dictionary<string, int> indexCounts)
+        {
+            return new Dictionary<string, string>
+            {
+                { m_Setting.GetSettingsLocaleID(), "Silence XP" },
+                { m_Setting.GetOptionTabLocaleID(Setting.kSection), "Main" },
+
+                { m_Setting.GetOptionGroupLocaleID(Setting.kToggleGroup), "Toggle enable" },
+
+                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Toggle)), "Toggle enable" },
+                { m_Setting.GetOptionDescLocaleID(nameof(Setting.Toggle)), $"Use bool property with setter and getter to get toggable option" },
+            };
+        }
+
+        public void Unload() {
+        }
+    }
+}
